2b7981c9a54e1ab90f16495cee71f8a93a88bde9
ymat <drowse314@gmail.com>
2013-04-14 23:52:11 +0900

    Add extension for trafaret validation integration.

---
+++ froshki/ext/trafaret_attr.py
@@ -0,0 +1,56 @@
+# encoding: utf-8
+
+try:
+    import trafaret
+except ImportError:
+    raise ImportError('trafaret is not installed')
+from froshki.model import Attribute
+
+
+class TrafaretPoweredAttribute(Attribute):
+    """
+    froshki.model.Attribute subclass using trafaret validation
+    system.
+
+    classmethod validate(klass, input_value) is used for handling
+    trafaret validation, which is to be non-overridable.
+    """
+
+    trafaret = trafaret.Any()
+
+    @classmethod
+    def validate(klass, input_value):
+        try:
+            checked = klass.trafaret.check(input_value)
+            return True, checked
+        except trafaret.DataError as err:
+            return False, err.error
+
+
+def trafaret_attr(trafaret, name='TrafaretAttribute'):
+    """
+    trafaret_attr(trafaret) -> TrafaretPoweredAttribute subclass.
+
+    TrafaretPoweredAttribute subclass factory.
+    Usage:
+    >>> import trafaret
+    >>> from froshki import Froshki, validation_hook
+    >>> class SendInquiry(Froshki):
+    ...     user_name = trafaret_attr(trafaret.String())()
+    ...     user_contact = trafaret_attr(trafaret.Email())()
+    ...     user_contact_confirmation = trafaret_attr(trafaret.Email())()
+    ...     message = trafaret_attr(trafaret.String(regex=r'\w{,400}'))()
+    ...     @validation_hook
+    ...     def confirm_email(self):
+    ...         return self.user_contact == self.user_contact_confirmation
+    >>>
+    >>> send_inquiry = SendInquiry(user_name='yu mat', user_contact='drowse314@gmail.com', user_contact_confirmation='drowse314@gmail.com', message='cannot post messages to my group')
+    >>> send_inquiry.validate()
+    True
+    """
+    return type(
+        name, (TrafaretPoweredAttribute,),
+        dict(
+            trafaret=trafaret,
+        ),
+    )
---
+++ tests/test_ext_trafaret.py
@@ -0,0 +1,107 @@
+# encoding: utf-8
+
+import unittest
+import trafaret
+from froshki import Froshki, validation_hook
+from froshki.ext.trafaret_attr import TrafaretPoweredAttribute, trafaret_attr
+
+
+class TestTrafaretIntegration(unittest.TestCase):
+
+    def test_trafaret_powered_attribute(self):
+
+        class Int(TrafaretPoweredAttribute):
+            trafaret = trafaret.Int()
+        class Email(TrafaretPoweredAttribute):
+            trafaret = trafaret.Email()
+        class ProductId(TrafaretPoweredAttribute):
+            trafaret = trafaret.Enum('F171', 'F172', 'F173')
+        class OrderVolume(TrafaretPoweredAttribute):
+            trafaret = trafaret.Int(gt=0)
+
+        import datetime
+        datetime_converter = (
+            trafaret.String(regex=r'^(\d{4})/(\d{2})/(\d{2})$')
+            >> (lambda m: datetime.datetime(
+                    *[int(num) for num in m.groups()]
+                ))
+        )
+        class Date(TrafaretPoweredAttribute):
+            trafaret = datetime_converter
+
+        class OrderSubmit(Froshki):
+            client_id = Int()
+            client_email = Email()
+            product_id = ProductId()
+            volume = OrderVolume()
+            order_date = Date()
+            delivary_date = Date()
+            @validation_hook
+            def consistent_date(self):
+                try:
+                    return self.order_date < self.delivary_date
+                except:
+                    return False
+
+        attr_source = dict(
+            client_id=1249, client_email='drowse314@gmail.com',
+            product_id='F172', volume=1,
+            order_date='2013/04/16', delivary_date='2013/05/01',
+        )
+        order_submit = OrderSubmit(source=attr_source)
+        self.assertTrue(order_submit.validate())
+
+        failure_updates = dict(
+            client_id='ymat', client_email='drowse314:gmail.com',
+            product_id='F17', volume=-1,
+            order_date='2013/04/5', delivary_date='2012/05/01',
+        )
+        for failure_key in failure_updates:
+            order_submit = OrderSubmit(
+                source=attr_source.copy().update(
+                    dict(failure_key=failure_updates[failure_key])
+                )
+            )
+            self.assertFalse(order_submit.validate())
+
+    def test_trafaret_attribute_shortcut(self):
+
+        class EventEntry(Froshki):
+            user_id = trafaret_attr(trafaret.Int(gt=0))()
+            user_contact = trafaret_attr(trafaret.Email())()
+            team_members = trafaret_attr(
+                trafaret.String(regex=r'^\d[\d,]*$')
+                >> (lambda m: [int(i) for i in m.string.split(',')])
+            )()
+            team_name = trafaret_attr(trafaret.String())(nullable=True)
+            @validation_hook
+            def leader_in_team(self):
+                try:
+                    return self.user_id in self.team_members
+                except:
+                    return False
+
+        attr_source = dict(
+            user_id=3219, user_contact='jsai.ml.keeper.ymat@gmail.com',
+            team_members='314,6341,3219,4418',
+            team_name='Nice Middles',
+        )
+        event_entry = EventEntry(source=attr_source)
+
+        failure_updates = dict(
+            user_id='ymat', user_contact='y@m@a@t@example.com',
+            team_members='314,6341,4418',
+        )
+        for failure_key in failure_updates:
+            event_entry = EventEntry(
+                source=attr_source.copy().update(
+                    dict(failure_key=failure_updates[failure_key])
+                )
+            )
+            self.assertFalse(event_entry.validate())
+
+        # Nullable attribute.
+        valid_source = attr_source.copy()
+        valid_source.pop('team_name')
+        event_entry = EventEntry(source=valid_source)
+        self.assertTrue(event_entry.validate())
